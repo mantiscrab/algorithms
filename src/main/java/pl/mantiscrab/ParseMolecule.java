fd6dfa56ba455a8265db3515bda5a84eeccd317c
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-04-10 21:41:15 +0200

    Refactor: remove redundant methods and classes

--- src/main/java/pl/mantiscrab/ParseMolecule.java
+++ src/main/java/pl/mantiscrab/ParseMolecule.java
@@ -7,82 +7,57 @@ import java.util.Map;
 
 class ParseMolecule {
     private final NumberOfAtomsMap atoms = new NumberOfAtomsMap();
-    private final String formula;
+    private final List<Character> formulaList;
     private Integer index = 0;
 
     public ParseMolecule(String formula) {
-        this.formula = formula;
+        this.formulaList = stringToCharacterList(formula);
     }
-
 
     public static Map<String, Integer> getAtoms(String formula) {
         return new ParseMolecule(formula).parseMolecule();
     }
 
     private Map<String, Integer> parseMolecule() {
-        List<Character> formulaList = stringToCharacterList(formula);
-
-        for (; index < formulaList.size(); index++) {
-            int parseUnitFinishBound = getParseUnitFinishBound(formulaList);
-            List<Character> parseUnit = formulaList.subList(index, parseUnitFinishBound);
-            AtomSymbolAndNumber atom = parse(parseUnit);
-            atoms.addAtomOccurrences(atom.getName(), atom.getNumber());
-            index = parseUnitFinishBound - 1;
+        while (formulaHasCharactersLeft()) {
+            Map<String, Integer> parsedAtoms = parse();
+            atoms.addAtomOccurrences(parsedAtoms);
         }
         return atoms.getNumberOfAtoms();
     }
 
-    private int getParseUnitFinishBound(List<Character> formulaList) {
-        int internalIndex = index;
-        MyCharacter character = MyCharacter.of(formulaList.get(index));
-        if (character.isAlphabetic())
-            internalIndex++;
-        else throw new RuntimeException();
-        if (internalIndex < formulaList.size()
-                && MyCharacter.of(formulaList.get(internalIndex)).isLowerCase())
-            internalIndex++;
-        while (internalIndex < formulaList.size()
-                && MyCharacter.of(formulaList.get(internalIndex)).isDigit()) {
-            internalIndex++;
-        }
-        return internalIndex;
-    }
-
-    private AtomSymbolAndNumber parse(List<Character> formulaList) {
+    private Map<String, Integer> parse() {
         StringBuilder atomNameBuilder = new StringBuilder();
-        int i = 0;
-        final char ch1 = formulaList.get(i);
+        final char ch1 = formulaList.get(index);
         if (Character.isUpperCase(ch1)) {
             atomNameBuilder.append(ch1);
-            i++;
-        } else
-            throw new RuntimeException();
-        if (listHasIndex(formulaList, i)) {
-            final char ch2 = formulaList.get(i);
-            if (Character.isLowerCase(ch2)) {
-                atomNameBuilder.append(formulaList.get(i));
+            index++;
+            if (formulaHasCharactersLeft()) {
+                final char ch2 = formulaList.get(index);
+                if (Character.isLowerCase(ch2)) {
+                    atomNameBuilder.append(formulaList.get(index));
+                }
             }
+            String atomName = atomNameBuilder.toString();
+
+            StringBuilder atomNumberBuilder = new StringBuilder();
+            while (index < formulaList.size()
+                    && Character.isDigit(formulaList.get(index))) {
+                atomNumberBuilder.append(formulaList.get(index));
+                index++;
+            }
+            final int atomNumber = atomNumberBuilder.toString().equals("") ? 1 : Integer.parseInt(atomNumberBuilder.toString());
+            return Map.of(atomName, atomNumber);
         }
-        String atomName = atomNameBuilder.toString();
-
-        StringBuilder atomNumberBuilder = new StringBuilder();
-        while (i < formulaList.size()
-                && MyCharacter.of(formulaList.get(i)).isDigit()) {
-            atomNumberBuilder.append(formulaList.get(i));
-            i++;
-        }
-        final int atomNumber = atomNumberBuilder.toString().equals("") ? 1 : Integer.parseInt(atomNumberBuilder.toString());
-        return new AtomSymbolAndNumber(atomName, atomNumber);
+        throw new RuntimeException();
     }
-
-    private boolean listHasIndex(List<Character> formulaList, int internalIndex) {
-        return internalIndex < formulaList.size();
-    }
-
 
     private List<Character> stringToCharacterList(String formula) {
-        List<Character> formulaList = formula.chars().mapToObj(c -> (char) c).toList();
-        return formulaList;
+        return formula.chars().mapToObj(c -> (char) c).toList();
+    }
+
+    private boolean formulaHasCharactersLeft() {
+        return index < formulaList.size();
     }
 }
 
@@ -103,68 +78,9 @@ class NumberOfAtomsMap {
         return Collections.unmodifiableMap(numberOfAtoms);
     }
 
-}
-
-class MyCharacter {
-    private final Character character;
-
-
-    static MyCharacter of(Character character) {
-        return new MyCharacter(character);
-    }
-
-    MyCharacter(Character character) {
-        this.character = character;
-    }
-
-    boolean isAlphabetic() {
-        return Character.isAlphabetic(character);
-    }
-
-    boolean isUpperCase() {
-        return Character.isUpperCase(character);
-    }
-
-    boolean isLowerCase() {
-        return Character.isLowerCase(character);
-    }
-
-    @Override
-    public String toString() {
-        return character.toString();
-    }
-
-    public boolean isOpeningParenthesis() {
-        return character.equals('(') || character.equals('[') || character.equals('{');
-    }
-
-    public boolean isDigit() {
-        return Character.isDigit(character);
-    }
-
-    public Integer toDigit() {
-        return Integer.valueOf(character.toString());
-    }
-
-    public boolean isUpperCaseAlphabetic() {
-        return Character.isAlphabetic(character) && Character.isUpperCase(character);
-    }
-}
-
-class AtomSymbolAndNumber {
-    private final String name;
-    private final Integer number;
-
-    public AtomSymbolAndNumber(String name, Integer number) {
-        this.name = name;
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getNumber() {
-        return number;
+    public void addAtomOccurrences(Map<String, Integer> parsedAtoms) {
+        for (String key : parsedAtoms.keySet()) {
+            addAtomOccurrences(key, parsedAtoms.get(key));
+        }
     }
 }

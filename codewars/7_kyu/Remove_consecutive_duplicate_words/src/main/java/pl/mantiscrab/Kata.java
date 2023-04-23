c86394132e149bf605b70a7b4be57ed0b19bfc2c
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-04-23 21:41:32 +0200

    Refactor

--- codewars/7_kyu/Remove_consecutive_duplicate_words/src/main/java/pl/mantiscrab/Kata.java
+++ codewars/7_kyu/Remove_consecutive_duplicate_words/src/main/java/pl/mantiscrab/Kata.java
@@ -1,15 +1,7 @@
 package pl.mantiscrab;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public class Kata {
     public static String removeConsecutiveDuplicates(String s) {
-        final Set<String> words = Arrays.stream(s.split(" ")).collect(Collectors.toSet());
-        for (final String word : words) {
-            s = s.replaceAll("(^| )(%s( |$)){2,}".formatted(word), "$1$2");
-        }
-        return s.trim();
+        return s.replaceAll("(\\b\\S+)( \\1)+\\b", "$1");
     }
 }

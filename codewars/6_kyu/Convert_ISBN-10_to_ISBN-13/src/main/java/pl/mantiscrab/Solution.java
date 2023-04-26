b42b51f1f534c90d4912bf36444b490185a4f680
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-04-26 22:40:52 +0200

    Refactor

--- codewars/6_kyu/Convert_ISBN-10_to_ISBN-13/src/main/java/pl/mantiscrab/Solution.java
+++ codewars/6_kyu/Convert_ISBN-10_to_ISBN-13/src/main/java/pl/mantiscrab/Solution.java
@@ -1,21 +1,22 @@
 package pl.mantiscrab;
 
+import java.util.Arrays;
+
 public class Solution {
     public static String isbnConverter(String isbn) {
-        final String isbnWithPrefixAddedSuffixRemoved = isbn.replaceFirst("(.*)[\\dX]$", "978-$1");
-        final int checkDigit = countCheckDigit(isbnWithPrefixAddedSuffixRemoved);
-        return isbnWithPrefixAddedSuffixRemoved + checkDigit;
+        final String isbn12 = isbn.replaceFirst("(.*)[\\dX]$", "978-$1");
+        final int checkDigit = countCheckDigit(isbn12);
+        return isbn12 + checkDigit;
     }
 
-    private static int countCheckDigit(final String isbnWithPrefixAddedAndSuffixRemoved) {
-        final String numbersWithoutHyphens = isbnWithPrefixAddedAndSuffixRemoved.replaceAll("-", "");
-        final String[] split = numbersWithoutHyphens.split("");
+    private static int countCheckDigit(final String isbn12) {
+        final String isbn12WithoutHyphens = isbn12.replaceAll("-", "");
+        final Integer[] integers = Arrays.stream(isbn12WithoutHyphens.split(""))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
         int numberSum = 0;
-        for (int i = 0; i < split.length; i++) {
-            if (i % 2 == 0)
-                numberSum += Integer.parseInt(split[i]);
-            else
-                numberSum += Integer.parseInt(split[i]) * 3;
+        for (int i = 0; i < integers.length; i++) {
+            numberSum += (i % 2 == 0) ? integers[i] : integers[i] * 3;
         }
         final int modulo = numberSum % 10;
         return modulo == 0 ? 0 : 10 - modulo;

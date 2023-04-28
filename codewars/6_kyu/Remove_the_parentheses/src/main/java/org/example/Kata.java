5ae8ce10492f2410942373d161e677f42d0265a9
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-04-28 22:47:00 +0200

    Codewars/6 kyu/remove the parentheses (#11)
    * Remove_the_parentheses
    
    * codewars/6_kyu/Remove_the_parentheses

---
+++ codewars/6_kyu/Remove_the_parentheses/src/main/java/org/example/Kata.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Kata {
+    public static String removeParentheses(final String str) {
+        String result = str;
+        while (result.contains("(")
+                && result.contains(")")
+                && result.indexOf("(") < result.lastIndexOf(")")) {
+            result = result.replaceAll("\\([^()]*?\\)", "");
+
+        }
+        return result;
+    }
+}

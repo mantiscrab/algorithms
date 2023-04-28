c5bd3dc06a918a58f4888c8c366b786d3c43a4a5
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-04-28 23:40:33 +0200

    For loop simplified

--- codewars/6_kyu/Convert_string_to_camel_case/src/main/java/pl/mantiscrab/Solution.java
+++ codewars/6_kyu/Convert_string_to_camel_case/src/main/java/pl/mantiscrab/Solution.java
@@ -7,15 +7,13 @@ import java.util.List;
 class Solution {
 
     static String toCamelCase(String s) {
-        final List<String> words = Arrays.asList(s.split("[_-]"));
-        final ArrayList<String> strings = new ArrayList<>();
-        strings.add(words.get(0));
+        final List<String> words = Arrays.asList(s.split("[_-](\\w)"));
+        final ArrayList<String> result = new ArrayList<>();
+        result.add(words.get(0));
         for (int i = 1; i < words.size(); i++) {
             final String word = words.get(i);
-            final String firstLetter = word.length() == 0 ? "" : Character.valueOf(word.charAt(0)).toString();
-            final String capitalizeWord = word.replaceFirst(firstLetter, firstLetter.toUpperCase());
-            strings.add(capitalizeWord);
+            result.add(word.substring(0, 1).toUpperCase() + word.substring(1));
         }
-        return String.join("", strings);
+        return String.join("", result);
     }
 }

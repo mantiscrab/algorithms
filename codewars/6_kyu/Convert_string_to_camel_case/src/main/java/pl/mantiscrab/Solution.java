59b43bdf58babbc20b2277e46168948c5cf75046
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-04-28 23:41:19 +0200

    Convert_string_to_camel_case

--- codewars/6_kyu/Convert_string_to_camel_case/src/main/java/pl/mantiscrab/Solution.java
+++ codewars/6_kyu/Convert_string_to_camel_case/src/main/java/pl/mantiscrab/Solution.java
@@ -1,19 +1,10 @@
 package pl.mantiscrab;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Pattern;
 
 class Solution {
 
     static String toCamelCase(String s) {
-        final List<String> words = Arrays.asList(s.split("[_-](\\w)"));
-        final ArrayList<String> result = new ArrayList<>();
-        result.add(words.get(0));
-        for (int i = 1; i < words.size(); i++) {
-            final String word = words.get(i);
-            result.add(word.substring(0, 1).toUpperCase() + word.substring(1));
-        }
-        return String.join("", result);
+        return Pattern.compile("[_-](\\w)").matcher(s).replaceAll(r -> r.group(1).toUpperCase());
     }
 }
